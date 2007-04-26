ff39763bbab15d0007007ffeaea7e52748dd75aa
nlp <nlp@localhost>
2007-04-26 16:25:18 +0200

    [svn r20] Initial XML config implementation, added example config

--- Main.cpp
+++ Main.cpp
@@ -6,6 +6,185 @@
 
 using namespace std;
 
+bool convertMD2Mesh( TiXmlElement *configNode, bool convertCoordinates )
+{
+	printf( "Doing MD2 Mesh conversion\n" );
+			
+	return false;
+}
+
+bool convertMD3Mesh( TiXmlElement *configNode, bool convertCoordinates )
+{
+	TiXmlHandle configHandle( configNode );
+	printf( "Doing MD3 Mesh conversion\n" );
+	
+	// Find input and output file names
+	TiXmlElement *inputFileNode = configHandle.FirstChild( "inputfile" ).FirstChild( "filename" ).ToElement();
+	if ( !inputFileNode )
+	{
+		printf( "Error: No input file defined\n" );
+		return false;
+	}
+	TiXmlElement *outputFileNode = configHandle.FirstChild( "outputfile" ).FirstChild( "filename" ).ToElement();
+	if ( !outputFileNode )
+	{
+		printf( "Error: No output file defined\n" );
+		return false;
+	}
+	
+	const char *inputFile = inputFileNode->GetText();
+	const char *outputFile = outputFileNode->GetText();
+	
+	// Try to load the MD3 file
+	MD3Structure md3Struct;
+	if ( !md3Struct.load( inputFile ) )
+	{
+		printf( "Error: Could not load input file '%s'\n", inputFile );
+		return false;
+	}
+	
+	// Process animations
+	AnimationList animList;
+	
+	// Try to load animations from an animation file
+	TiXmlHandle animFileHandle = configHandle.FirstChild( "animationfile" );
+	TiXmlHandle animFilenameHandle = animFileHandle.FirstChild( "filename" );
+	TiXmlHandle animGroupHandle = animFileHandle.FirstChild( "animationgroup" );
+	if ( animFileHandle.ToElement() && animFilenameHandle.ToElement() && animGroupHandle.ToElement() )
+	{
+		const char *animFile = animFilenameHandle.ToElement()->GetText();
+		
+		AnimationLoader animLoader;
+		if ( animLoader.load( animFile ) )
+		{
+			const AnimationList &(AnimationLoader::*retrievalFunc)() = NULL;
+			TiXmlElement *animGroupNode = animGroupHandle.FirstChild().ToElement();
+			if ( animGroupNode )
+			{
+				const string &value = animGroupNode->ValueStr();
+				if ( value == "grouplower" )
+					retrievalFunc = &AnimationLoader::getLowerAnimations;
+				else if ( value == "groupupper" )
+					retrievalFunc = &AnimationLoader::getUpperAnimations;
+			}
+			
+			if ( retrievalFunc )
+			{
+				const AnimationList &loadedAnims = (animLoader.*retrievalFunc)();
+				if ( animFileHandle.FirstChild( "convertall" ).ToElement() )
+				{
+					printf( "Converting all animations from animation file\n" );
+					
+					for ( AnimationList::const_iterator i = loadedAnims.begin();
+						i != loadedAnims.end(); ++i )
+					{
+						animList.push_back( *i );
+					}
+				}
+				else
+				{
+					// TODO
+					printf( "Converting individual animations\n" );
+				}
+			}
+			else
+			{
+				printf( "Warning: No animation group given for animation file\n" );
+			}
+		}
+		else
+		{
+			printf( "Warning: Could not load animation file '%s'\n", animFile );
+		}
+	}
+	
+	// TODO process animation defs
+	
+	// TODO process materials
+	
+	FILE *f = fopen( outputFile, "w" );
+	if ( !f )
+	{
+		printf( "Error: Could not write to file '%s'\n", outputFile );
+		return false;
+	}
+	
+	string str = Q3ModelToMesh( md3Struct, animList, 0, true );
+	fputs( str.c_str(), f );
+	fclose( f );
+		
+	return true;
+}
+
+int main( int argc, char **argv )
+{
+	if ( argc < 2 )
+	{
+		printf( "Usage: QuakeToOgre [config file]\n" );
+		return 1;
+	}
+	
+	TiXmlDocument config( argv[1] );
+	if ( !config.LoadFile() )
+	{
+		printf( "Error: Could not load configuration from file '%s', reason:\n", argv[1] );
+		printf( "Error %i on row %i column %i:\n%s\n", config.ErrorId(), 
+			config.ErrorRow(), config.ErrorCol(), config.ErrorDesc() );
+		return 1;
+	}
+	
+	const TiXmlElement *root = config.RootElement();
+	if ( !root || root->ValueStr() != "conversion" )
+	{
+		printf( "Error: This is not a valid QuakeToOgre configuration file\n" );
+		return 1;
+	}
+	
+	bool convertCoordinates = false;
+	bool (*conversionFunc)( TiXmlElement *, bool ) = NULL;
+	const TiXmlElement *conversionNode = NULL;
+	
+	for ( const TiXmlNode *child = root->FirstChild(); child; child = child->NextSibling() )
+	{
+		const TiXmlElement *node = child->ToElement();
+		if ( !node )
+			continue;
+			
+		const string &name = node->ValueStr();
+		if ( name == "convertcoordinates" )
+			convertCoordinates = true;	
+		else if ( name == "md2mesh" )
+		{
+			conversionFunc = &convertMD2Mesh;
+			conversionNode = node;
+		}
+		else if ( name == "md3mesh" )
+		{
+			conversionFunc = &convertMD3Mesh;
+			conversionNode = node;
+		}
+	}
+
+	if ( !conversionFunc || !conversionNode )
+	{
+		printf( "Error: No conversion method found\n" );
+		return 1;
+	}
+
+	// Casting away const is evil, but it is necessary if we want to use TiXmlHandles
+	if ( conversionFunc( const_cast<TiXmlElement*>(conversionNode), convertCoordinates ) )
+	{
+		printf( "Conversion succeeded!\n" );
+		return 0;
+	}
+	else
+	{
+		printf( "Conversion failed...\n" );
+		return 1;
+	}
+}
+
+#if 0
 int main( int argc, char **argv )
 {
 	if ( argc < 4 )
@@ -58,3 +237,4 @@ int main( int argc, char **argv )
 
 	return 0;
 }
+#endif
