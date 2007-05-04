d49fa75315f57d2181b821fac1b48f0ace36e481
nlp <nlp@localhost>
2007-05-04 10:47:51 +0200

    [svn r38] 'Documented' validation script

--- validate.php
+++ validate.php
@@ -1,4 +1,8 @@
 #!/usr/bin/php
+/*
+Run this script with the PHP Command Line Interface to validate a 
+configuration XML file against its Document Type Definition.
+*/
 <?php
 if ( count( $argv ) < 2 )
 {
